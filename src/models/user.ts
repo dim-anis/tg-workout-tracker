34869e49eff2b38e4ac655b08faec2e64f86f88e
dim-anis <anis.dim@gmail.com>
2023-04-04 17:01:57 +0400

    remove blank lines

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-
-// import {models, model, Schema, type Types} from 'mongoose';
 import mongoose from 'mongoose';
 import {WorkoutSchema, Workout, type WorkoutType} from './workout';
 import {ExerciseSchema, type ExerciseType} from './exercise';
