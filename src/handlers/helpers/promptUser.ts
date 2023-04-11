328ca7ea305651d940e4c9ce09b4cd3e12f8ce04
dim-anis <anis.dim@gmail.com>
2023-04-11 17:16:48 +0400

    add more informative error messages to user input prompts

--- src/handlers/helpers/promptUser.ts
+++ src/handlers/helpers/promptUser.ts
@@ -1,48 +1,99 @@
 import {type MyContext, type MyConversation} from 'types/bot';
 
-export function validateWeight(input: string): boolean {
-	const parsedInput = parseFloat(input.toString());
+const errorMessages = {
+	input_is_not_yes_or_no: '\n\n❌ <b>Input must be "Yes" or "No"</b>',
+	input_too_long: '\n\n❌ <b>Input is too long. Max length is 50 characters</b>',
+	input_contains_special_chars: 'Input contains special characters. Only alphanumeric characters are allowed',
+	input_is_not_a_number: '\n\n❌ <b>Input must be a number</b>',
+	input_is_out_of_range_weight: '\n\n❌ <b>Input is out of range. Must be between 1 and 999</b>',
+	input_is_out_of_range_rpe: '\n\n❌ <b>Input is out of range. Must be between 6.0 and 10.0</b>',
+	input_is_out_of_range_repetitions: '\n\n❌ <b>Input is out of range. Must be between 1 and 99</b>',
+	input_is_not_a_multiple_of_half: '\n\n❌ <b>Input must be a multiple of 0.5 (5.5, 6, 6.5, etc.)/b>',
+	input_is_not_an_integer: '\n\n❌ <b>Input must be an integer</b>\n',
+};
 
-	return parsedInput >= 1 && parsedInput <= 999 && !isNaN(parsedInput);
+function updateMessageWithError(message: string, error = '') {
+	const existingErrorMessages = message.split('\n\n').filter(msg => msg.startsWith('❌'));
+	const timestamp = new Date().toLocaleTimeString();
+	const errorMsg = error ? errorMessages[error as keyof typeof errorMessages] + '\n' + timestamp : '';
+	const newMessage = existingErrorMessages.length > 0 ? message.replace('\n\n' + existingErrorMessages.join('\n\n'), errorMsg) : message + errorMsg;
+
+	return newMessage;
 }
 
-export function validateReps(input: string): boolean {
-	const parsedInput = parseInt(input.toString(), 10);
-
-	return Number.isInteger(parsedInput) && parsedInput >= 1 && parsedInput <= 999 && !isNaN(parsedInput);
-}
-
-export function validateRPE(input: string): boolean {
-	const floatValue = parseFloat(input.toString());
-	if (isNaN(floatValue)) {
-		return false;
+export function validateWeight(input: string): string | undefined {
+	const parsedInput = parseFloat(input);
+	if (isNaN(parsedInput)) {
+		return 'input_is_not_a_number';
 	}
 
-	return floatValue >= 6.0 && floatValue <= 10.0 && floatValue % 0.5 === 0;
+	if (parsedInput < 1 || parsedInput > 999) {
+		return 'input_is_out_of_range_weight';
+	}
+
+	return undefined;
 }
 
-export function validateExerciseName(exerciseName: string): boolean {
-	if (!exerciseName || exerciseName.trim().length > 50) {
-		return false;
+export function validateReps(input: string): string | undefined {
+	const parsedInput = parseInt(input, 10);
+	if (isNaN(parsedInput)) {
+		return 'input_is_not_a_number';
+	}
+
+	if (!Number.isInteger(parsedInput)) {
+		return 'input_is_not_an_integer';
+	}
+
+	if (parsedInput < 1 || parsedInput > 99) {
+		return 'input_is_out_of_range_repetitions';
+	}
+
+	return undefined;
+}
+
+export function validateRPE(input: string): string | undefined {
+	const floatValue = parseFloat(input);
+	if (isNaN(floatValue)) {
+		return 'input_is_not_a_number';
+	}
+
+	if (floatValue < 6.0 || floatValue > 10.0) {
+		return 'input_is_out_of_range_rpe';
+	}
+
+	if (floatValue % 0.5 !== 0) {
+		return 'input_is_not_a_multiple_of_half';
+	}
+
+	return undefined;
+}
+
+export function validateExerciseName(input: string): string | undefined {
+	if (!input || input.trim().length > 50) {
+		return 'input_too_long';
 	}
 
 	const regex = /^[a-zA-Z0-9\s'".!?()_-]+$/;
-	if (!regex.test(exerciseName.trim())) {
-		return false;
+	if (!regex.test(input.trim())) {
+		return 'input_contains_special_chars';
 	}
 
-	return true;
+	return undefined;
 }
 
-export function validateYesNoInput(input: string): boolean {
+export function validateYesNoInput(input: string): string | undefined {
 	const normalizedInput = input.toLowerCase().trim();
-	return normalizedInput === 'yes' || normalizedInput === 'no';
+	if (normalizedInput !== 'yes' && normalizedInput !== 'no') {
+		return 'input_is_not_yes_or_no';
+	}
+
+	return undefined;
 }
 
 export async function promptUserForNumber(
 	ctx: MyContext,
 	conversation: MyConversation,
-	validatorFunc: (input: string) => boolean,
+	validatorFunc: (input: string) => string | undefined,
 	chat_id: number,
 	message_id: number,
 	message: string,
@@ -58,20 +109,20 @@ export async function promptUserForNumber(
 
 	await ctx.api.deleteMessage(ctx.chat!.id, ctx.message!.message_id);
 
-	const isInputValid = validatorFunc(ctx.message!.text!);
+	const validationError = validatorFunc(ctx.message!.text!);
 
-	if (isInputValid && typeof ctx.message!.text !== 'undefined') {
+	if (!validationError && typeof ctx.message!.text !== 'undefined') {
 		return Number(ctx.message!.text);
 	}
 
-	const errorMsg = `\n\n❌ <b>Must be a number! Try again</b>\n${new Date().toLocaleTimeString()}`;
-	return promptUserForNumber(ctx, conversation, validatorFunc, chat_id, message_id, message + errorMsg, options);
+	const newMessage = updateMessageWithError(message, validationError);
+	return promptUserForNumber(ctx, conversation, validatorFunc, chat_id, message_id, newMessage, options);
 }
 
 export async function promptUserForText(
 	ctx: MyContext,
 	conversation: MyConversation,
-	validatorFunc: (input: string) => boolean,
+	validatorFunc: (input: string) => string | undefined,
 	chat_id: number,
 	message_id: number,
 	message: string,
@@ -88,14 +139,14 @@ export async function promptUserForText(
 
 	await ctx.api.deleteMessage(ctx.chat!.id, ctx.message!.message_id);
 
-	const isInputValid = validatorFunc(ctx.message!.text!);
+	const validationError = validatorFunc(ctx.message!.text!);
 
-	if (isInputValid && typeof ctx.message!.text !== 'undefined') {
+	if (!validationError && typeof ctx.message!.text !== 'undefined') {
 		return ctx.message!.text;
 	}
 
-	const errorMsg = `\n\n❌ <b>Invalid text input! Try again</b>\n${new Date().toLocaleTimeString()}`;
-	return promptUserForText(ctx, conversation, validatorFunc, chat_id, message_id, message + errorMsg, options);
+	const newMessage = updateMessageWithError(message, validationError);
+	return promptUserForText(ctx, conversation, validatorFunc, chat_id, message_id, newMessage, options);
 }
 
 export async function promptUserForWeight(
